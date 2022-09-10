475773cb0efafb5dfa7b45487c847ce46d761431
Yisi Liu <85250232+yisliu@users.noreply.github.com>
2022-09-10 03:40:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int k, n;
+  cin>>n>>k;
+  int h = n/16;
+  int l = n%16;
+  if(l>=10){
+    cout<<h<<char(l-10+'A');
+  }
+  else if(l<10){
+    cout<<h<<char(l+'0');
+  }
+}
